9cb2f2b6afa8e23ffc7f01aaa26dd406cc093f65
KaioJansen <kaio.jansen@outlook.com>
2025-04-09 01:48:11 -0300

    Add environment variables configuration

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDOn03xMtJps0zoDcZsdPIe3kKFmk1Z0DE",
-  authDomain: "sexmatch-d0bfe.firebaseapp.com",
-  projectId: "sexmatch-d0bfe",
-  storageBucket: "sexmatch-d0bfe.firebasestorage.app",
-  messagingSenderId: "1065119815678",
-  appId: "1:1065119815678:web:50a1b6f3a61cadfa7412af",
-  measurementId: "G-QSLDCG7EX4"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
